d854487be222d10b310733392ad921dc2d7ec1b6
sngeza <geza.simon@dpc.hu>
2022-05-23 12:11:38 +0200

    copyright messages. unneeded _callback

--- tutorial/embedded-login/index.js
+++ tutorial/embedded-login/index.js
@@ -1,12 +1,10 @@
 /*
- * @forgerock/javascript-sdk
+ * Copyright (c) 2022 ForgeRock. All rights reserved.
  *
- * index.html
- *
- * Copyright (c) 2020 ForgeRock. All rights reserved.
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
+
 
 const FATAL = 'Fatal';
 
