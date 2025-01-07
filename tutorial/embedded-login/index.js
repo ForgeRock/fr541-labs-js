83fefcad3d1f927855a369c946c63145dc304990
Likvid2 <nicolas.sipek@gmail.com>
2025-01-07 11:48:19 +0100

    update to 4.6.0

--- tutorial/embedded-login/index.js
+++ tutorial/embedded-login/index.js
@@ -155,14 +155,9 @@ const handlers = {
     const isLocationRequired = deviceCollectorCB.isLocationRequired();
     const isMetadataRequired = deviceCollectorCB.isMetadataRequired();
 
-    const device = new forgerock.FRDevice /* DONE CUSTOMDEVICE */({
-      // Collect just the presence of Arial and Helvetica
-      fontNames: ["Arial", "Helvetica"],
-      // Do not collect any display properties
-      displayProps: [],
-      // Just collect User Agent
-      browserProps: ["userAgent"],
-    });
+    const device = new forgerock.FRDevice /* TODO CUSTOMDEVICE */(
+      
+    );
 
     const profile = await device.getProfile({
       location: isLocationRequired,
